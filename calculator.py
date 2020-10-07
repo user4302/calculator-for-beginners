758e2f79a5bc6272991e9563ad30466b46674488
Angelo <32593782+user4302@users.noreply.github.com>
2020-10-07 09:49:08 +0530

    Update calculator.py
    added add and subtract function, removed option to select multiply and divide

--- calculator.py
+++ calculator.py
@@ -13,5 +13,19 @@ def divide(x, y):
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
+
+while True:
+    choice = input("Enter choice(1/2/3/4): ")
+
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+        break
+    else:
+        print("Invalid Input")
