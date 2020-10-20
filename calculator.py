ac79517bc9755256643896833d2b338de348f1a3
st3shy <43742170+st3shy@users.noreply.github.com>
2020-10-20 11:44:48 +0530

    Update calculator.py
    added LCM for 2 numbers

--- calculator.py
+++ calculator.py
@@ -5,33 +5,42 @@ def add(x, y):
     return x + y
 
 # This function subtracts two numbers
-
-
 def subtract(x, y):
     return x - y
 
 # This function multiplies two numbers
-
-
 def multiply(x, y):
     return x * y
 
 # This function divides two numbers
-
-
 def divide(x, y):
     if y != 0:
         return x / y  # denominator can't be zero
     else:
         return 'inf'
-
+    
+# This function finds the L.C.M. of two input number
+def compute_lcm(x, y):
+    # choose the greater number
+    if x > y:
+       greater = x
+    else:
+       greater = y
+    
+    while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+    return lcm
 
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
-print("5.Exit")
+print("5.LCM of 2 numbers")
+print("0.Exit")
 
 while True:
     # Take input from the user
@@ -53,7 +62,11 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
-    elif choice == '5':
+            
+        elif choice == '5':
+            print(num1, "/", num2, "=", compute_lcm(num1, num2))
+            
+    elif choice == '0':
         print('Thanks for using')
         break
     else:
